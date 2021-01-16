4691c23f824b73e31131cc60fb5088999c6af8e8
xiaozhi001 <18638389375@163.com>
2021-01-16 14:29:58 +0800

    链接

---
+++ src/views/category/Category.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>我是分类</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Category'
+}
+</script>
+<style scoped>
+  
+</style>
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>我是个人档案</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile'
+}
+</script>
+<style scoped>
+  
+</style>
